3521d8080aa3ca25e007c0f1e19311500a8dae64
eng.muhammad <Muhammedkhaleda@gmail.com>
2023-05-10 16:29:47 +0300

    AzkarScreen.kt
    UI

--- app/src/main/java/com/muhkhaled/wazzakrine/azkarFeature/presentation/AzkarScreen.kt
+++ app/src/main/java/com/muhkhaled/wazzakrine/azkarFeature/presentation/AzkarScreen.kt
@@ -3,9 +3,9 @@ package com.muhkhaled.wazzakrine.azkarFeature.presentation
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Share
@@ -13,21 +13,36 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.muhkhaled.wazzakrine.R
+import com.muhkhaled.wazzakrine.core.presentation.theme.Shapes
 import com.muhkhaled.wazzakrine.core.presentation.theme.WazzakrineTheme
 
 @Composable
 fun AzkarScreen() {
-
+    Column(modifier = Modifier.fillMaxSize()) {
+        TodayDua(Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp))
+        AzkarCategory(Modifier.weight(1F).padding(16.dp))
+    }
 }
 
+
+@Preview
+@Composable
+fun AzkarScreenPrev() {
+    WazzakrineTheme {
+        MaterialTheme(shapes = Shapes.copy(medium = RoundedCornerShape(8))) {
+            AzkarScreen()
+        }
+    }
+}
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -70,8 +85,9 @@ fun TodayDua(modifier: Modifier = Modifier) {
                         top = 8.dp,
                         bottom = 16.dp
                     ),
-                    text = "..(الًبقتم الًمعو ابًيط اقًزرو اعًفان امًلع كلُأسأ ينإ مَّهللا)",
-                    fontSize = 24.sp
+                    text = "اللَّهمَّ إنِّي أسألُكَ عِلمًا نافعًا ورزقًا طيِّبًا وعملًا متقبَّلًا",
+                    fontSize = 24.sp,
+                    textAlign = TextAlign.Center
                 )
                 Divider(
                     modifier = Modifier
@@ -91,40 +107,138 @@ fun TodayDua(modifier: Modifier = Modifier) {
                         Text(modifier = Modifier.padding(start = 8.dp), text = "Share")
                     }
                 }
-
             }
         }
     }
-
-    val items = listOf(AzkarScreens)
-    LazyVerticalStaggeredGrid(columns = StaggeredGridCells.Fixed(2), content = {
-    })
 }
 
-
-data class AzkarItem(
-    @DrawableRes val resourceId: Int,
-    val size: Dp,
-    val name: String,
-    val type: AzkarScreens,
-)
-
-sealed class AzkarScreens {
-    companion object ss {
-        object MorningZeker : AzkarScreens()
-        object EveningZeker : AzkarScreens()
-        object PrayZeker : AzkarScreens()
-        object TravelZeker : AzkarScreens()
-        object EatingZeker : AzkarScreens()
-        object SleepZeker : AzkarScreens()
-    }
-}
 
 @Preview
 @Composable
 fun TodaysDuaPrev() {
     WazzakrineTheme {
         TodayDua()
+    }
+}
+
+@Composable
+fun AzkarCategory(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1F),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ZekerItem(
+                modifier = Modifier.weight(1F),
+                resourceId = R.drawable.morinig_img,
+                zekerName = "Morning"
+            )
+            Column(
+                modifier = Modifier
+                    .weight(1F)
+                    .padding(start = 8.dp)
+            ){
+                Row(
+                    modifier = Modifier.weight(1F),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    ZekerItem(
+                        modifier = Modifier.weight(1F),
+                        resourceId = R.drawable.travel_img,
+                        zekerName = "Travel"
+                    )
+                    ZekerItem(
+                        modifier = Modifier
+                            .weight(1F)
+                            .padding(start = 8.dp),
+                        resourceId = R.drawable.food_img,
+                        zekerName = "Food & Drink"
+                    )
+                }
+                ZekerItem(
+                    modifier = Modifier
+                        .weight(1F)
+                        .padding(top = 8.dp),
+                    resourceId = R.drawable.sleep_img,
+                    zekerName = "Sleep"
+                )
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1F)
+                .padding(top = 8.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ZekerItem(
+                modifier = Modifier.weight(1F),
+                resourceId = R.drawable.pray_img,
+                zekerName = "Pray"
+            )
+            ZekerItem(
+                modifier = Modifier
+                    .weight(1F)
+                    .padding(start = 8.dp),
+                resourceId = R.drawable.evening_img,
+                zekerName = "Evening"
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AzkarCategoryPrev() {
+    WazzakrineTheme {
+        MaterialTheme(shapes = Shapes.copy(medium = RoundedCornerShape(8))) {
+            AzkarCategory()
+        }
+    }
+}
+
+
+@Composable
+fun ZekerItem(
+    modifier: Modifier = Modifier,
+    @DrawableRes resourceId: Int,
+    zekerName: String,
+) {
+    Card(modifier = modifier) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .paint(
+                    painter = painterResource(id = resourceId),
+                    contentScale = ContentScale.FillBounds
+                )
+        ) {
+            Text(
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .padding(8.dp),
+                text = zekerName,
+                fontSize = 14.sp,
+                color = Color.White
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ZekarItemPrev() {
+    WazzakrineTheme {
+        ZekerItem(
+            modifier = Modifier.size(width = 162.dp, height = 174.dp),
+            resourceId = R.drawable.evenning_azkar_bg,
+            zekerName = "Morning"
+        )
     }
 }
 
